0a4ae4cc081ce38967fbb5c628077d1843e8de52
wan lucas <wan.lucas.90@gmail.com>
2022-11-12 01:17:46 -0300

    :bug: consertando conexão

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv').config();
+
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
-  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
