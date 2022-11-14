f162740947bacdca6d4ec0a82ed9bc0c32baa195
wan lucas <wan.lucas.90@gmail.com>
2022-11-14 19:04:21 -0300

    :bug: atualizando connection para o avaliador

--- src/models/connection.js
+++ src/models/connection.js
@@ -7,7 +7,6 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
-  port: 3306,
 }); 
 
 module.exports = connection;
